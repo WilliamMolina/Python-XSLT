de1ed0cd04ea22517117ff5b147249634a7fa938
laurqugu <laurajquinchia@gmail.com>
2019-04-17 13:13:35 -0500

    microservicio que recibe un XML, realiza la transformación mediante un XSLT y devuelve otro XML, lo envía a un rest y lo almacena en S3

--- runner.py
+++ runner.py
@@ -1,7 +1,46 @@
 import lxml.etree as ET
+import xmltodict, dicttoxml
+import os, datetime, io, json
+import boto3
+import requests
+from pytz import timezone
 
-dom = ET.parse("input/house.xml")
-xslt = ET.parse("XSLT/transformation.xsl")
-transform = ET.XSLT(xslt)
-newdom = transform(dom)
-print(ET.tostring(newdom, pretty_print=False))
+#set timezone
+tz = timezone('America/Bogota')
+fmt = "%Y%m%d%H%M%S"
+fmt_elastic = "%Y-%m-%dT%H:%M:%S-05:00"
+
+s3 = boto3.client('s3')
+
+BUCKET = os.getenv('BUCKET')
+
+def transxslt(event, content):
+
+    obj = ET.fromstring(event['body'])
+
+    #dom = ET.parse(obj)
+    xslt = ET.parse("XSLT/transformation.xsl")
+    transform = ET.XSLT(xslt)
+    newdom = transform(obj)
+    res= ET.tostring(newdom, pretty_print=False)
+    response= res.decode("utf-8")
+    
+    r = requests.post('https://j2dihjgwn9.execute-api.us-east-1.amazonaws.com/dev/xml', data = response)
+
+    save(r.text)
+
+    return {
+        'statusCode':200,
+        'body': response,
+        'headers': {
+            'Content-Type': 'application/xml',
+        }
+    }
+
+def save(response):
+    fechaHora = datetime.datetime.now(tz).strftime(fmt)  
+    s3.put_object(
+        Bucket=BUCKET,
+        Key=f'{fechaHora}.xml',
+        Body=response,
+    )
