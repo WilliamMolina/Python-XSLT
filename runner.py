733707a7601cfd32b97cbe2aab4b12f1a9c1c923
laurqugu <laurajquinchia@gmail.com>
2019-04-24 09:53:18 -0500

    convertir rest a SOAP

--- runner.py
+++ runner.py
@@ -44,3 +44,28 @@ def save(response):
         Key=f'{fechaHora}.xml',
         Body=response,
     )
+
+def post_xml(event, context):
+    namespaces = {
+        "https://www.w3schools.com/xml/":None,
+        "http://www.w3.org/2001/XMLSchema-instance":None,
+        "http://www.w3.org/2001/XMLSchema":None,
+        "http://schemas.xmlsoap.org/soap/envelope/":None,
+        "http://prueba.com.co/serviciosoap/v1": None
+    }
+    obj = xmltodict.parse(event['body'], process_namespaces=True, namespaces=namespaces)
+    return respuesta_soap(obj["Envelope"]["Body"]["rentalProperties"])
+
+    #return obj
+
+def respuesta_soap(resp):
+    body_start = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:v1=\"http://prueba.com.co/serviciosoap/v1\"><soap:Body><v1:rentalPropertiesResponse>"
+    body_end = "</v1:rentalPropertiesResponse></soap:Body></soap:Envelope>"
+    my_item_func = lambda x: None  
+    return {
+        'statusCode':200,
+        'body': body_start + dicttoxml.dicttoxml(resp, attr_type=False, root=False, item_func=my_item_func).replace(b'<None>',b'').replace(b'</None>',b'').decode("utf-8") + body_end,
+        'headers': {
+            'Content-Type': 'application/xml',
+        }
+    }
